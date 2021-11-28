20a8ccb5190de920eb07051a4e2ae3fe9d43bf0f
Thiago Galvao <thiagosilva128@gmail.com>
2021-11-28 12:04:14 -0300

    cleaning code

--- app.js
+++ app.js
@@ -13,14 +13,19 @@ app.post('/youtube', async (request, response) => {
     try {
         const url = request.body.url
 
+        const isValidUrl = ytdl.validateURL(url)
+        if(!isValidUrl) {
+            throw new Error('Url invalid')
+        
+        }
+
         const info = await ytdl.getBasicInfo(url)
         
         const { title, viewCount, thumbnails, lengthSeconds } = info.videoDetails
-        
     
         response.send({ url, title, viewCount, lengthSeconds, thumbnail: thumbnails[thumbnails.length - 1].url })
     } catch (error) {
-        alert('Something went wrong', error)
+        response.status(400).send({ error })
     }
 })
 
@@ -28,7 +33,8 @@ app.get('/download', async (request, response) => {
     try {
         const url = request.query.url
     
-        const info = await ytdl.getBasicInfo(url)
+        console.log("test1")
+        let info = await ytdl.getBasicInfo(url)
         const { title } = info.videoDetails
         const hash = crypto.randomBytes(4)
 
@@ -37,11 +43,23 @@ app.get('/download', async (request, response) => {
         ytdl(url, {
             format: 'mp4'
         }).pipe(response)
+        
+        console.log("test2")
+        
 
     } catch (error) {
         response.status(404).send({ error: 'Something went wrong, verify the url and try again' })
     }
  
+})
+
+app.get('/audio', async (request, response) => {
+     const url = request.query.url
+
+     const info = await ytdl.getInfo(url)
+     const audioFormats = ytdl.filterFormats(info.formats, 'audioonly')
+
+     console.log(audioFormats)
 })
 
 app.listen(PORT)
